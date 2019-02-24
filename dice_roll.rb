49068e680cbe7ceb38b6376076f6c1dd439b1b4c
John Cho <johnycho16@gmail.com>
2019-02-24 03:57:41 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,15 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+# def roll
+#   return rand 1..6
+# end
+
+
 def roll
-  # code goes here
+  array = (1..6).to_a
+  rand_index = rand 0..5
+  return array[rand_index]
 end
+
+roll
